3acd05a6643764e9582cf119777340f4bafb74a3
Mihai <mihai.ocneanu@gmail.com>
2021-11-26 15:45:52 +0200

    wip

--- src/bulma/Modal.vue
+++ src/bulma/Modal.vue
@@ -1,20 +1,20 @@
 <template>
-    <core-modal v-bind="$attrs"
-        :transition-duration="transitionDuration">
-        <template #default="{ close }">
-            <fade>
-                <div class="modal is-active">
-                    <div class="modal-background"/>
-                    <div class="modal-content">
-                        <slot/>
-                    </div>
+    <fade>
+        <div class="modal is-active">
+            <div class="modal-background"/>
+            <div class="modal-content">
+                <slot/>
+            </div>
+            <core-modal v-bind="$attrs"
+                :transition-duration="transitionDuration">
+                <template #default="{ close }">
                     <button class="modal-close is-large"
                         aria-label="close"
                         @click="close"/>
-                </div>
-            </fade>
-        </template>
-    </core-modal>
+                </template>
+            </core-modal>
+        </div>
+    </fade>
 </template>
 
 <script>
